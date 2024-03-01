58fdfde396c8791a22217a29ddda06da5c3c342f
Lucy Ge <lucy.ge@alluxio.com>
2024-03-01 13:47:36 -0800

    try writing to local disk perf

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,8 +1,13 @@
 use clap::{crate_version, Arg, ArgAction, Command};
 use fuser::{FileAttr, FileType, Filesystem, MountOption, ReplyAttr, ReplyData, ReplyDirectory, ReplyEntry, Request, ReplyOpen, ReplyWrite};
 use libc::ENOENT;
+use libc::EIO;
 use std::ffi::OsStr;
+use std::fs::{File, OpenOptions};
+use std::io::{Seek, SeekFrom, Write};
 use std::time::{Duration, UNIX_EPOCH};
+use std::sync::atomic::{AtomicU16, AtomicU8, Ordering};
+use log::error;
 
 const TTL: Duration = Duration::from_secs(1); // 1 second
 
@@ -45,6 +50,8 @@ const HELLO_TXT_ATTR: FileAttr = FileAttr {
 };
 
 struct HelloFS;
+
+
 
 impl Filesystem for HelloFS {
     fn lookup(&mut self, _req: &Request, parent: u64, name: &OsStr, reply: ReplyEntry) {
@@ -132,9 +139,23 @@ impl Filesystem for HelloFS {
         _lock_owner: Option<u64>,
         reply: ReplyWrite,
     ) {
-        log::info!("LUCY write!");
-        reply.written(_data.len() as u32);
-        //reply.error(ENOSYS);
+        log::info!("LUCY write!off:{}:len:{}", _offset, _data.len());
+        match OpenOptions::new()
+            .write(true)
+            .create(true) // Optionally create the file if it doesn't exist
+            .open(format!("{}/{}", HelloFS::WRITEBACK_DIR, "abc"))
+        {
+            Ok(mut file) => {
+                // file.seek( SeekFrom::Current(_offset));
+                file.write_all(_data);
+                // file.sync_all();
+                reply.written(_data.len() as u32);
+            },
+            Err(e) => {
+                error!("{}", e);
+                reply.error(EIO);
+            }
+        }
     }
 
     fn mknod(
@@ -148,11 +169,55 @@ impl Filesystem for HelloFS {
         reply: ReplyEntry
     ) {
         log::info!("LUCY mknod!");
-        reply.entry(&TTL, &HELLO_TXT_ATTR, 0);
+        match File::create( format!("{}/{}", HelloFS::WRITEBACK_DIR, "abc")) {
+            Ok(mut file) => {
+                let file_attr = FileAttr {
+                    ino: 2,
+                    size: 13,
+                    blocks: 1,
+                    atime: UNIX_EPOCH, // 1970-01-01 00:00:00
+                    mtime: UNIX_EPOCH,
+                    ctime: UNIX_EPOCH,
+                    crtime: UNIX_EPOCH,
+                    kind: FileType::RegularFile,
+                    perm: 0o644,
+                    nlink: 1,
+                    uid: 501,
+                    gid: 20,
+                    rdev: 0,
+                    flags: 0,
+                    blksize: 512,
+                };
+                reply.entry(&TTL, &file_attr, 0);
+            },
+            Err(e) => {
+                error!("{}", e);
+                reply.error(EIO);
+            }
+        }
     }
 }
 
+impl HelloFS {
+    const WRITEBACK_DIR: &'static str = "/mnt-nvme2/writeback";
+
+}
+
+fn testfunc1() {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true) // Optionally create the file if it doesn't exist
+        .open(format!("/tmp/{}", "abc"))
+        .expect("Failed to open file");
+    let data = String::from("LUCY-");
+    file.seek(SeekFrom::Current(0));
+    file.write_all(data.as_bytes());
+    file.sync_all();
+}
+
 fn main() {
+    // testfunc1();
+
     let matches = Command::new("hello")
         .version(crate_version!())
         .author("Christopher Berner")
@@ -177,7 +242,7 @@ fn main() {
         .get_matches();
     env_logger::init();
     let mountpoint = matches.get_one::<String>("MOUNT_POINT").unwrap();
-    let mut options = vec![MountOption::RO, MountOption::FSName("hello".to_string())];
+    let mut options = vec![MountOption::RW, MountOption::FSName("hello".to_string())];
     if matches.get_flag("auto_unmount") {
         options.push(MountOption::AutoUnmount);
     }
